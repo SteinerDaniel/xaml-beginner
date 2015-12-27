dcb168d65c96204589530b205584b24851729972
SteinerDaniel <daniel.steiner@edu.fh-joanneum.at>
2015-12-27 09:52:32 +0100

    project commit
    homework 1

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            this.OrderItems = base.Repository.Orders;
+        }
+
+        private List<Order> _OrderItems;
+
+        public List<Order> OrderItems
+        {
+            //get { return base.Repository.Orders; }
+            get { return _OrderItems; }
+            set
+            {
+                if (value != _OrderItems)
+                {
+                    _OrderItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
