b9839f23eb5371b9534b354996f72d7d1911132e
SteinerDaniel <daniel.steiner@edu.fh-joanneum.at>
2015-12-30 23:23:34 +0100

    not sure how to implement point 5

---
+++ RestaurantManager.Models/ViewModels/ExpediteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Models.ViewModels
+{
+    public class ExpediteViewModel : ExpediteDataManager
+    {
+        public string SpecialRequests { get; set; }
+
+        public Table Table { get; set; }
+
+        public bool Complete { get; set; }
+
+        public bool Expedite { get; set; }
+
+        public List<Order> OrderItem { get; set; }
+    }
+}
